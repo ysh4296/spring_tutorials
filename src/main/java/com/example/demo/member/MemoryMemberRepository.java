676d2288cc68d79dcff7bfb6f39996aa175fbe65
cadi <dbtmdgns4296@naver.com>
2025-04-12 14:44:00 +0900

    member non null exception 추가

--- src/main/java/com/example/demo/member/MemoryMemberRepository.java
+++ src/main/java/com/example/demo/member/MemoryMemberRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.member;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MemoryMemberRepository implements MemberRepository {
@@ -8,11 +9,15 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public void save(Member member) {
+        Objects.requireNonNull(member, "Member cannot be null.");
+        Objects.requireNonNull(member.getId(), "Member ID cannot be null.");
         store.put(member.getId(), member); // 저장 구현
     }
 
     @Override
     public Member findById(Long memberId) {
+        Member member = store.get(memberId);
+        Objects.requireNonNull(member, "Member cannot be null.");
         return store.get(memberId); // 값 가져오기 구현
     }
 }
