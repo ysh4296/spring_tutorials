9841ab3f04bc00f18b1df10aee87552853db42f2
cadi <dbtmdgns4296@naver.com>
2025-04-27 18:38:13 +0900

    어노테이션 추가

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -8,17 +8,24 @@ import com.example.demo.member.MemberServiceImpl;
 import com.example.demo.member.MemoryMemberRepository;
 import com.example.demo.order.OrderService;
 import com.example.demo.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
